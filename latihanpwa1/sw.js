7c34d5d68c08d9c94f573674e8dbf0193ed6a1a7
Dien <dien_kamal@ymail.com>
2020-10-10 22:16:00 +0700

    Menyimpan Cache Secara Dinamis

--- latihanpwa1/sw.js
+++ latihanpwa1/sw.js
@@ -15,7 +15,8 @@ self.addEventListener("install", function(event) {
 // Working Offline
 self.addEventListener("fetch", function(event) {
     event.respondWith(
-      caches.match(event.request).then(function(response) {
+      caches.match(event.request, {cacheName:CACHE_NAME})
+      .then(function(response) {
         console.log("ServiceWorker: Menarik data: ", event.request.url);
    
         if (response) {
@@ -27,7 +28,36 @@ self.addEventListener("fetch", function(event) {
           "ServiceWorker: Memuat aset dari server: ",
           event.request.url
         );
-        return fetch(event.request);
+        var fetchRequest = event.request.clone();
+        return fetch(fetchRequest).then(
+          function (response) {
+            if (!response || response.status !== 200) {
+              return response;
+            }
+            var responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(function (cache) {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          }
+        );
       })
     );
   });
+
+  // Menghapus Cache Lama
+self.addEventListener('activate', function(event) {
+  console.log('Aktivasi service worker baru');
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME && cacheName.startsWith("codepolitan-reader-lite")) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
