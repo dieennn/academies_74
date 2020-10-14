7910b75015ea09ff3441aa419fbe07b8303198cf
Dien <dien_kamal@ymail.com>
2020-10-14 23:46:46 +0700

    Menampilkan Notifikasi dengan Actions

--- pre/modul7/notif.js
+++ pre/modul7/notif.js
@@ -57,3 +57,28 @@ function showNotifikasiBadge() {
         console.error('Fitur notifikasi tidak diijinkan.');
     }
 }
+
+// Menampilkan Notifikasi dengan Actions
+function showNotifikasiActions() {
+    const title = 'Notifikasi dengan Actions';
+    const options = {
+        'body': 'Ini adalah konten notifikasi dengan pilihan actions.',
+        'actions': [
+            {
+                'action': 'yes-action',
+                'title': 'Ya',
+            },
+            {
+                'action': 'no-action',
+                'title': 'Tidak',
+            }
+        ]
+    };
+    if (Notification.permission === 'granted') {
+        navigator.serviceWorker.ready.then(function(registration) {
+            registration.showNotification(title, options);
+        });
+    } else {
+        console.error('Fitur notifikasi tidak diijinkan.');
+    }
+}
